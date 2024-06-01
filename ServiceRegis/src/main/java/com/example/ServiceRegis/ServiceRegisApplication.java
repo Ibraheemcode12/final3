b37443ec9b601a79830e5004923e35e0781cbc6c
Ibraheemcode12 <chosenhunter430@gmail.com>
2024-06-01 16:06:53 +0300

    service

---
+++ ServiceRegis/src/main/java/com/example/ServiceRegis/ServiceRegisApplication.java
@@ -0,0 +1,17 @@
+package com.example.ServiceRegis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceRegisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegisApplication.class, args);
+		
+	}
+	
+	
+}
